8f2a088672ab95ffb659cbecd0a8d90e174b8047
Arina Pristupa <pristupa.arina2018@yandex.by>
2022-11-28 20:09:17 +0300

    added nojekyll

---
+++ scripts/index.js
@@ -0,0 +1,30 @@
+let profileButton = document.querySelector('.profile__img-pen');
+let popup = document.querySelector('.popup');
+let popupClose = document.querySelector('.popup__close');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+let popupForm = document.querySelector('.popup__form');
+let popupTextName = document.querySelector('.popup__text_name');
+let popupTextHobby = document.querySelector('.popup__text_hobby');
+
+function openPopup() {
+    popup.classList.add('popup_opened');
+    popupTextName.value = profileTitle.textContent;
+    popupTextHobby.value = profileSubtitle.textContent;
+}
+
+function closePopup() {
+    popup.classList.remove('popup_opened');
+}
+
+function formSubmitHandler(event) {
+    event.preventDefault();
+    profileTitle.textContent = popupTextName.value;
+    profileSubtitle.textContent = popupTextHobby.value;
+    closePopup();
+}
+
+profileButton.addEventListener('click', openPopup);
+popupClose.addEventListener('click', closePopup);
+
+popupForm.addEventListener('submit', formSubmitHandler);

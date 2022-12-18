fb297552810d760273b9ec34ae3ff1cf04f12210
Arina Pristupa <pristupa.arina2018@yandex.by>
2022-12-18 13:16:09 +0300

    added nojekyll

--- scripts/index.js
+++ scripts/index.js
@@ -1,61 +1,30 @@
-const initialCards = [
-  {
-    name: "Архыз",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-  },
-  {
-    name: "Челябинская область",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-  },
-  {
-    name: "Иваново",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-  },
-  {
-    name: "Камчатка",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-  },
-  {
-    name: "Холмогорский район",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-  },
-  {
-    name: "Байкал",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-  },
-];
+const profileButton = document.querySelector(".profile__img-pen"); //картинка с ручкой
+const profileAddButton = document.querySelector(".profile__add-button"); //кнопка с плюсиком что-то добавить
 
-let profileButton = document.querySelector(".profile__img-pen"); //картинка с ручкой
-let profileAddButton = document.querySelector(".profile__add-button"); //кнопка с плюсиком что-то добавить
-let popup = document.querySelector(".popup");
+const popupCloseButtonList = document.querySelectorAll(".popup__close"); // крестик закрыть попапы все
 
-//let popupClose = document.querySelector('.popup__close'); // крестик закрыть попапы
-let popupCloseAll = document.querySelectorAll(".popup__close"); // крестик закрыть попапы все
+const profileTitle = document.querySelector(".profile__title"); //название в профайле жак
+const profileSubtitle = document.querySelector(".profile__subtitle"); // хобби в профайле иследователь
 
-let profileTitle = document.querySelector(".profile__title"); //название в профайле жак
-let profileSubtitle = document.querySelector(".profile__subtitle"); // хобби в профайле иследователь
-//let popupForm = document.querySelector('.popup__form'); // попап форма общая
+const profilePopup = document.querySelector(".popup-save"); //первая попап сохранить
+const profileForm = document.querySelector(".popup__form_popup-save"); // попап форма сохранить
+const popupTextName = document.querySelector(".popup__text_type_name"); // имя в форме попап сохранить
+const popupTextHobby = document.querySelector(".popup__text_type_hobby"); //хобби в форме попап сохранить
 
-let savePopup = document.querySelector(".popup-save"); //первая попап сохранить
-let popupFormSave = document.querySelector(".popup__form_popup-save"); // попап форма сохранить
-let popupTextName = document.querySelector(".popup__text_type_name"); // имя в форме попап сохранить
-let popupTextHobby = document.querySelector(".popup__text_type_hobby"); //хобби в форме попап сохранить
+const cardPopup = document.querySelector(".popup-create"); //второй попап добавить
+const popupFormCreate = document.querySelector(".popup__form_popup-create"); // попап форма добавить
+const popupTextTitle = document.querySelector(".popup__text_type_title"); // название в форме попап добавить
+const popupTextImgLink = document.querySelector(".popup__text_type_img-link"); //ссылка в форме попап добавить
 
-let createPopup = document.querySelector(".popup-create"); //второй попап добавить
-let popupFormCreate = document.querySelector(".popup__form_popup-create"); // попап форма добавить
-let popupTextTitle = document.querySelector(".popup__text_type_title"); // название в форме попап добавить
-let popupTextImgLink = document.querySelector(".popup__text_type_img-link"); //ссылка в форме попап добавить
+const imgPopup = document.querySelector(".popup-img"); //третий попап картинка в большом размере
+const imgPopupImage = document.querySelector(".popup-img__image"); //третий попап картинка в нём большая
+const imgPopupTitle = document.querySelector(".popup-img__title"); //третий попап название того что на картинке
 
-let imgPopup = document.querySelector(".popup-img"); //третий попап картинка в большом размере
-let imgPopupImage = document.querySelector(".popup-img__image"); //третий попап картинка в нём большая
-let imgPopupTitle = document.querySelector(".popup-img__title"); //третий попап название того что на картинке
-
-const elementsCards = document.querySelector(".elements"); //все элементы в карточка то что гридом все карточки
-const templateCard = document.querySelector("#card").content; // темплейт в нтмл
+const cardsContainer = document.querySelector(".elements"); //все элементы в карточка то что гридом все карточки
+const templateCard = document.querySelector("#card").content.querySelector(".element"); // темплейт в нтмл
 
 function addCard(name, link) {
-  const elementCard = templateCard.querySelector(".element").cloneNode(true);
-
+  const elementCard = templateCard.cloneNode(true);
   const elementImg = elementCard.querySelector(".element__img");
   const elementTrash = elementCard.querySelector(".element__trash");
   const elementImgLike = elementCard.querySelector(".element__img-like");
@@ -65,17 +34,32 @@ function addCard(name, link) {
   elementImg.src = link;
   elementImg.alt = name;
 
-  elementImg.addEventListener("click", () => cardImageBig(elementImg, name)); //по клику вызывается функция которая карточка становится на весь экран
-  elementTrash.addEventListener("click", () => cardDelete(elementTrash)); //по клику вызывается функция которая удаляет карточку
-  elementImgLike.addEventListener("click", () => cardLike(elementImgLike)); //по клику вызывается функция переключение класса тоггле
+  elementImg.addEventListener("click", () => handleOpenCard(elementImg, name)); //по клику вызывается функция которая карточка становится на весь экран
+  elementTrash.addEventListener("click", () => handleCardDelete(elementTrash)); //по клику вызывается функция которая удаляет карточку
+  elementImgLike.addEventListener("click", () => handleCardLike(elementImgLike)); //по клику вызывается функция переключение класса тоггле
+
+  return elementCard;
+}
+
+//добавляет карточку в начало сетки
+const renderCard = function (name, link) {
+  cardsContainer.prepend(addCard(name, link));
+}
+
+//метод forEACH
+function renderCards(cards) {
+  cards.forEach(function ({ name, link }) {
+    renderCard(name, link);
+  });
+}
 
 //функция удаления
-function cardDelete(element) {
+function handleCardDelete(element) {
   element.closest(".element").remove();
 }
-  
+
 //функция открытие карточки на весь экран
-function cardImageBig(image, title) {
+function handleOpenCard(image, title) {
   openPopup(imgPopup);
   imgPopupImage.src = image.src;
   imgPopupImage.alt = title;
@@ -83,23 +67,9 @@ function cardImageBig(image, title) {
 }
 
 //функция лайка, переключение класса
-function cardLike(like) {
+function handleCardLike(like) {
   like.classList.toggle("element__img-like_active");
 }
-
-  elementsCards.prepend(elementCard); //добавляет карточку в начало сетки
-}
-console.log(addCard);
-
-//метод forEACH
-function defoltCards(cards) {
-  cards.forEach( function ({name, link}) {
-    addCard(name, link);
-  });
-}
-
-defoltCards(initialCards);
-
 //открытие попапа
 function openPopup(popup) {
   popup.classList.add("popup_opened");
@@ -110,45 +80,49 @@ function closePopup(popup) {
   popup.classList.remove("popup_opened");
 }
 
-//первый попап при клике сначало должно быть
-profileButton.addEventListener("click", function () {
-  popupTextName.value = profileTitle.textContent;
-  popupTextHobby.value = profileSubtitle.textContent;
-  openPopup(savePopup);
-});
 //первый попап при нажатии на кнопку сохранить
-function formSubmitHandler(event) {
+function handleProfileFormSubmit(event) {
   event.preventDefault();
 
   profileTitle.textContent = popupTextName.value;
   profileSubtitle.textContent = popupTextHobby.value;
 
-  closePopup(savePopup);
+  closePopup(profilePopup);
 }
 
-//второй попап при открытии
-profileAddButton.addEventListener("click", function () {
-  popupFormCreate.reset(); // в форму идёт по умолчанию
-  openPopup(createPopup);
-});
 //второй попап при нажатии кнопки добавить
-function formSubmitAddCard(event) {
+function handleCardFormSubmit(event) {
   event.preventDefault();
 
   const name = popupTextTitle.value;
   const link = popupTextImgLink.value;
 
-  addCard(name, link);
+  renderCard(name, link);
 
-  closePopup(createPopup);
+  closePopup(cardPopup);
 }
 
-popupFormSave.addEventListener("submit", formSubmitHandler);
-popupFormCreate.addEventListener("submit", formSubmitAddCard);
-
 //ипсользуем метод forEach закрытие попап
-popupCloseAll.forEach( function (button) {
+popupCloseButtonList.forEach(function (button) {
   button.addEventListener("click", function (event) {
     closePopup(event.currentTarget.closest(".popup")); //определяет элемент в котором обрабатывается событие, ищет ближайший подходящий класс
   });
 });
+
+//первый попап при клике сначало должно быть
+profileButton.addEventListener("click", function () {
+  popupTextName.value = profileTitle.textContent;
+  popupTextHobby.value = profileSubtitle.textContent;
+  openPopup(profilePopup);
+});
+
+//второй попап при открытии
+profileAddButton.addEventListener("click", function () {
+  popupFormCreate.reset(); // в форму идёт по умолчанию
+  openPopup(cardPopup);
+});
+
+profileForm.addEventListener("submit", handleProfileFormSubmit);
+popupFormCreate.addEventListener("submit", handleCardFormSubmit);
+
+renderCards(initialCards);
